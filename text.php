48aa708492e267847c7eca4e8927dbfb6d33ff55
mahipal0015 <mahipal001573985@gmail.com>
2018-06-30 19:55:04 +0530

    commited text.php

--- text.php
+++ text.php
@@ -1,1 +1,13 @@
-text.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>git hub</title>
+</head>
+<body>
+	<h3>Git</h3>
+	<p>git is distributed version control system</p>
+	<h3>GitHub</h3>
+	<p>github is website which use to provide GUI to manipulate project directly without using any code</p>
+	<b>NOTE : </b>GIT is not equal to GITHUB both are different 
+</body>
+</html>
